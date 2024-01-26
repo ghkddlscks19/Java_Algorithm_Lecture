6a3cc70667c0cf13528e833b0a0af6d573c11328
황인찬 <ghkddlscks19@naver.com>
2024-01-26 15:23:10 +0900

    조합수(메모이제이션)

---
+++ DFSAndBFS/Combination.java
@@ -0,0 +1,33 @@
+package DFSAndBFS;
+
+import java.util.Scanner;
+
+public class Combination {
+
+    static int[][] arr;
+
+    public int DFS(int n, int r) {
+        if (arr[n][r] > 0) {
+            return arr[n][r];
+        }
+        if (n == r || r == 0) {
+            return 1;
+        } else {
+            return arr[n][r] = DFS(n - 1, r - 1) + DFS( n - 1, r);
+        }
+    }
+
+    public static void main(String[] args) {
+        Combination t = new Combination();
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int r = sc.nextInt();
+
+        arr = new int[n+1][r+1];
+
+        System.out.println(t.DFS(n, r));
+
+
+    }
+}
