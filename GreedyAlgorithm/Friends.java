df5a7a3ae93a3c4ca093d22233a0e47ea42ec9fd
황인찬 <ghkddlscks19@naver.com>
2024-02-09 14:42:31 +0900

    친구인가? (Disjoint-Set : Union&Find)

---
+++ GreedyAlgorithm/Friends.java
@@ -0,0 +1,46 @@
+package GreedyAlgorithm;
+
+import java.util.Scanner;
+
+public class Friends {
+
+    public static int find(int v) {
+        if(v==unf[v]) return unf[v];
+        else return unf[v] = find(unf[v]);
+    }
+
+    public static void union(int a, int b) {
+        int fa = find(a);
+        int fb = find(b);
+        if(fa!=fb) unf[fa] = fb;
+    }
+
+    static int unf[];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        unf = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            unf[i] = i;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            union(a, b);
+        }
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int fa = find(a);
+        int fb = find(b);
+        if(fa==fb) System.out.println("YES");
+        else System.out.println("NO");
+    }
+}
